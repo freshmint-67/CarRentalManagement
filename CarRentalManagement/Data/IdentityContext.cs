d983f51e4ec9a0d635477114045a51f3c4b426ac
karum <karum@DESKTOP-T42B9AI>
2025-11-26 12:05:01 +0800

    Added database and seed data

---
+++ CarRentalManagement/Data/IdentityContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Data;
+
+namespace CarRentalManagement.Data
+{
+    public class IdentityContext(DbContextOptions<IdentityContext> options) : IdentityDbContext<CarRentalManagementUser>(options)
+    {
+    }
+}
